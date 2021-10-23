e00f9267c4ad879083b1269eed118123ff68c549
jabbourromulo <romulo.jabbour@wteco.net>
2021-10-22 20:02:38 -0400

    Classe do Telegram

--- Telegram.py
+++ Telegram.py
@@ -1,20 +1,29 @@
 import json, telepot, time
+from os import name
 from telepot.loop import MessageLoop
+from Chatbot import Joquinha
 
 with open("Token.json") as jsonToken:
     token = json.load(jsonToken)
 
 telegram = telepot.Bot(token)
+bot = Joquinha("Joquinha")
 
-def handle(msg):
+def receiveMessage(msg):
+    frase = bot.listen(phrase=msg["text"])
+    resposta = bot.think(frase)
+
+    bot.speak(resposta)
+    
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
-    if content_type == 'text':
-        #telegram.sendMessage(chat_id, msg['text'])
-        telegram.sendMessage(chat_id, "E ai?")
+    telegram.sendMessage(chat_id, resposta)
+    #if content_type == 'text':
+        ##telegram.sendMessage(chat_id, msg['text'])
+        #telegram.sendMessage(chat_id, "E ai?")
 
-MessageLoop(telegram, handle).run_as_thread()
+MessageLoop(telegram, receiveMessage).run_as_thread()
 print ('Listening ...')
 
 # Keep the program running.
