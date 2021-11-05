e72166c53092ada675789b656db9960ed7a3d1ed
jabbourromulo <romulo.jabbour@wteco.net>
2021-11-05 19:44:40 -0400

    novas features to chatbot

--- Chatbot.py
+++ Chatbot.py
@@ -10,26 +10,35 @@ class Joquinha():
             memory = open(name + '.json', 'r')
         except FileNotFoundError:
             memory = open(name + '.json', 'w')
-            memory.write('[["Joquinha"], {"oi": "Olá! Qual seu nome?", "tchau": "Tchau! Tchau!"}]')
+            memory.write(
+                            '''
+                                [   ["Joquinha"],
+                                       {
+                                           "oi": "Olá! Qual seu nome?",
+                                           "tchau": "Tchau! Tchau!",
+                                           "/lista": "O que você gostaria de adicionar na sua lista?",
+                                           "/matematica": "Que conta você quer fazer?",
+                                           "/contato": "Que telefone você gostaria de obter?"
+                                       },
+                                    []
+                                ]
+                            '''
+                        )
             memory.close()
             memory = open(name + '.json', 'r')
 
         self.name = name
-        self.known, self.phrases = json.load(memory)
+        self.known, self.phrases, self.lista = json.load(memory)
         memory.close()
         self.historic = [None]
     
     def listen(self, phrase=None):
-        if phrase == None:
-            phrase = input('Digite aqui: ')
-        phrase = str(phrase).lower()
-#        phrase = phrase.lower()
-        return phrase
+        return phrase.lower()
 
     def think(self, phrase):
         def execute(phrase):
             platform = sys.platform
-            command = phrase.replace('executa ', '')
+            command = phrase.replace('/executa ', '')
             if 'win' in platform:
                 os.startfile(command)
             if 'linux' in platform:
@@ -41,23 +50,29 @@ class Joquinha():
         if phrase in self.phrases:
             return self.phrases[phrase]
         # harcoded
-        if phrase == 'aprende':
+        if phrase == '/aprende':
             return 'O que você quer que eu aprenda?'
-        if phrase == 'pesquisa':
+        if phrase == '/pesquisa':
             return "www.google.com"
         #if phrase == 'executa':
         #    return execute()
-        if 'executa ' in phrase:
+        if '/executa ' in phrase:
             execute(phrase)
             return "Ta aí meu!!"
-
+            #/executa www.cnbc.com
         # historic
         lastPhrase = self.historic[-1]
         if lastPhrase == 'Olá! Qual seu nome?':
             name = self.getName(phrase)
             response = self.answerName(name)
             return response
-            
+        #if lastPhrase == 'O que você gostaria de adicionar na sua lista?':
+        #    while True:
+        #        listen()
+        #        if phrase != "sair":
+        #            self.lista.append(phrase)
+        #            self.saveMemory()
+        #        return self.lista
         if lastPhrase == 'O que você quer que eu aprenda?':
             self.key = phrase
             return 'Digite o que eu devo responder:'
@@ -93,7 +108,7 @@ class Joquinha():
     
     def saveMemory(self):
         memory = open(self.name + '.json', 'w')
-        json.dump([self.known, self.phrases], memory)
+        json.dump([self.known, self.phrases, self.lista], memory)
         memory.close()
                     
     def speak(self, phrase):
